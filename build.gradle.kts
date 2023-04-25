299a14feca03dd144e26f353f52e2d0252c19f47
Nazar Krylov <nazar.krylov@atola.com>
2023-04-25 15:15:43 +0300

    Building material

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin("jvm") version "1.8.20"
+  application
+  id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+val vertxVersion = "4.4.1"
+val junitJupiterVersion = "5.9.2"
+
+val mainVerticleName = "launch.MainVerticle"
+val launcherClassName = "io.vertx.core.Launcher"
+
+val watchForChange = "src/**/*"
+val doOnChange = "${projectDir}/gradlew classes"
+
+application {
+  mainClass.set(launcherClassName)
+}
+
+dependencies {
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0-Beta")
+
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0-rc2")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2")
+
+  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation("io.vertx:vertx-core")
+  implementation("io.vertx:vertx-web")
+  implementation("io.vertx:vertx-lang-kotlin")
+  implementation("io.vertx:vertx-lang-kotlin-coroutines:4.4.0")
+  implementation(kotlin("stdlib-jdk8"))
+  testImplementation("io.vertx:vertx-junit5")
+
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "11"
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles()
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+}
